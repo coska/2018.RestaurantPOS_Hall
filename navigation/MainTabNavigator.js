c87749e88612eabec3563579103bbce1ff4e7049
Sean Lee <yihsean@gmail.com>
2018-06-28 00:23:10 -0400

    Add new screen 'TableScreen'

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,9 +3,11 @@ import { createStackNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
+import TableScreen from '../screens/TableScreen';
 
 const HomeStack = createStackNavigator({
   Login: LoginScreen,
+  Table: TableScreen,
   Home: HomeScreen
 }, {
   navigationOptions: {
