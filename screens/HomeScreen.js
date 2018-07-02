c6ce9e1347f23e5eeda0b01c16299bd83197f6b6
Jihyun Yoon <nickyoon89@gmail.com>
2018-07-02 13:44:56 -0400

    no message

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,6 +13,25 @@ import {
 import { connect } from 'react-redux';
 import { getCategories, getProducts } from '../store/home/actions';
 
+var itemdata =[ 
+  { item: "Avocado Roll",
+    qty: 1,
+    price: 14
+  },
+  { item: "Beef Teriyaki",
+    qty: 1,
+    price: 25
+  },
+  { item: "Asahi Beer",
+    qty: 1,
+    price: 5
+  },
+  { item: "Green Tea",
+    qty: 2,
+    price: 1
+  }
+]
+
 class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
@@ -33,7 +52,102 @@ class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.sidebar}>
-          <View style={{flex: 1}}></View>
+          <View style={{flex: 1}}>
+          <FlatList
+              initialNumToRender={3}
+              ListHeaderComponent={()=>{
+                return (
+                  <View style={{flex:1, flexDirection:'row', borderColor: '#0000', backgroundColor: '#aaa', marginTop: 5}}>
+                    <View style={{ flex: 5, margin: 1}}>
+                      <Text style={{flex: 1, textAlignVertical:'center', color:'#eee'}}>Item</Text>
+                    </View>
+                    <View style={{ flex: 1, margin: 1}}>
+                      <Text style={{flex: 1, textAlign:'center', textAlignVertical:'center', color:'#eee'}}>Qty</Text>
+                    </View>
+                    <View style={{ flex: 2, margin: 1}}>
+                      <Text style={{flex: 1, textAlign:'center', textAlignVertical:'center', color:'#eee'}}>Unit</Text>
+                      <Text style={{flex: 1, textAlign:'center', textAlignVertical:'center', color:'#eee'}}>Price</Text>
+                    </View>
+                    <View style={{ flex: 2, margin: 1}}> 
+                      <Text style={{flex: 1, textAlign:'center', textAlignVertical:'center', color:'#eee'}}>Amount</Text>
+                    </View>
+                  </View>
+                );
+              }
+              }
+              data={itemdata}
+              numColumns={1}
+              renderItem={({ item}) => {
+                return (
+                  <View style={{flex:1, flexDirection:'row', backgroundColor: '#000'}}>
+                    <View style={{ flex: 3.5, marginHorizontal: 1}}>
+                      <Text style={{color:'#fff'}}>{item.item}</Text>
+                    </View>
+                    <View style={{ flex: 1.5, marginHorizontal: 1, flexDirection:'row'}}>
+                    <View style={{ flex: 1, marginHorizontal: 1}}>
+                        <TouchableOpacity onPress={()=>{}}>
+                          <Text style={{color:'#fff'}}>+</Text>
+                        </TouchableOpacity>
+                      </View>
+                      <View style={{ flex: 1, marginHorizontal: 1}}>
+                        <TouchableOpacity onPress={()=>{}}>
+                          <Text style={{color:'#fff'}}>-</Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                    <View style={{ flex: 1, marginHorizontal: 1}}>
+                      <Text style={{color:'#fff', textAlign:'center'}}>{item.qty}</Text>
+                    </View>
+                    <View style={{ flex: 2, marginHorizontal: 1}}>
+                      <Text style={{color:'#fff', textAlign:'right'}}>{item.price.toFixed(2)}</Text>
+                    </View>
+                    <View style={{ flex: 2, marginHorizontal: 1}}>
+                      <Text style={{color:'#fff', textAlign:'right'}}>{(item.qty*item.price).toFixed(2)}</Text>
+                    </View>
+                  </View>
+                );
+              }}
+              keyExtractor={(item, index) => index}
+              ListFooterComponent={() => {
+                var total=0;
+                var discount=0;
+                itemdata.forEach(element => {
+                  total+=element.price * element.qty;
+                });
+                return (
+                  <View style={{flex:4, backgroundColor: '#aaa', borderColor:'#0000'}}>
+                    <View style={{flex:1, flexDirection:'row'}}>
+                      <View style={{ flex: 2, marginHorizontal: 1}}>
+                        <Text style={{color:'#eee'}}>Subtotal:</Text> 
+                      </View>
+                      <View style={{ flex: 1, marginHorizontal: 1}}>
+                        <Text style={{textAlign:'right', color:'#eee'}}>{total.toFixed(2)}</Text>
+                      </View>
+                    </View>
+                    <View style={{flex:1, flexDirection:'row'}}>
+                      <View style={{ flex: 2, marginHorizontal: 1}}>
+                        <Text style={{color:'#eee'}}>Discount:</Text> 
+                      </View>
+                      <View style={{ flex: 1, marginHorizontal: 1}}>
+                        <Text style={{textAlign:'right', color:'#eee'}}>{discount.toFixed(2)}</Text>
+                      </View>
+                    </View>
+                    <View style={{flex:1, flexDirection:'row'}}>
+                      <View style={{ flex: 2, marginHorizontal: 1}}>
+                        <Text style={{color:'#eee'}}>Tax:</Text> 
+                      </View>
+                      <View style={{ flex: 1, marginHorizontal: 1}}>
+                        <Text style={{textAlign:'right', color:'#eee'}}>{(total*0.13).toFixed(2)}</Text>
+                      </View>
+                    </View>
+                    <View style={{ flex: 1, paddingHorizontal: 1, backgroundColor: '#555'}}>
+                    <Text style={{textAlign:'right', color:'#fff', fontSize:18, fontWeight:'bold'}}>{(total*1.13-discount).toFixed(2)}</Text>
+                    </View>
+                  </View>  
+                );
+              }}
+           />
+          </View>
         </View>
         <View style={styles.main}>
           <View style={{flex: 1}}>
