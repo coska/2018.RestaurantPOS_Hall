750dcc3623a9fc87bb74f67ea2ff8379c0f09654
Sean Lee <yihsean@gmail.com>
2018-07-31 20:32:52 -0400

    update endpoint from swagger to production

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-export const REST_ENDPOINT = 'https://virtserver.swaggerhub.com/coska/RestaurantPosV1/1.0.0';
+export const REST_ENDPOINT = 'http://35.231.163.51:8080';
+// https://virtserver.swaggerhub.com/coska/RestaurantPosV1/1.0.0
